0982ef84213244fc14b905ec89340dff7ebb9c21
Verso <vuoreol@pm.me>
2024-03-23 20:18:23 +0200

    fox

--- autoheader.js
+++ autoheader.js
@@ -132,6 +132,9 @@ fetch('config.json')
         // Replace the language prefix in the pathname with the desired language
         if (currentLang === "en") {
             newPath = newPath.replace("/en/", `/${language.toLowerCase()}/`);
+            if (language == "FI") {
+                newPath = newPath.replace("/en/", "/");
+            }
         } else {
             newPath = `/${language.toLowerCase()}${newPath}`;
         }
